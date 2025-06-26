58f4130156005ef72aac1adf34530d9a3130c19d
Quentin Feraboli <quentin.feraboli@softathome.com>
2025-06-26 10:32:37 +0200

    pwhm: wpa_supplicant: fix: removing wpa_s network specific channels conf params overridden by global freq_list
    This reverts commit 78c49637dbb8223ed84e4f8168bea9127d6fbb87.
    
    GitOrigin-RevId: 08f017eccebd1bf427c5ed072019c742155b755f
    
    Implements: PPM-3681
    References: FEAT-117
    Signed-off-by: Quentin Feraboli <quentin.feraboli@softathome.com>
    Signed-off-by: Ahmed Amine HAOUES <ahmed-amine.haoues_ext@softathome.com>

--- src/nl80211/wld_wpaSupp_cfgFile.c
+++ src/nl80211/wld_wpaSupp_cfgFile.c
@@ -64,7 +64,6 @@
 #include "wld_util.h"
 #include "wld_wps.h"
 #include "wld_endpoint.h"
-#include "wld_radio.h"
 #include "swl/map/swl_mapCharFmt.h"
 #include "wld_wpaSupp_cfgManager.h"
 #include "wld_wpaSupp_cfgManager_priv.h"
@@ -121,9 +120,14 @@ static swl_rc_ne s_setWpaSuppGlobalConfig(T_EndPoint* pEP, wld_wpaSupp_config_t*
     }
 
     char freqListStr[320] = {'\0'};
-    wld_rad_printPossibleFreqsWithSep(pRad, freqListStr, sizeof(freqListStr), " ");
+    for(int i = 0; i < pRad->nrPossibleChannels; i++) {
+        uint32_t freqMHz = 0;
+        swl_chanspec_t chanspec = SWL_CHANSPEC_NEW(pRad->possibleChannels[i], SWL_BW_20MHZ, pRad->operatingFrequencyBand);
+        if((SWL_RC_OK == swl_chanspec_channelToMHz(&chanspec, &freqMHz))) {
+            swl_strlst_catFormat(freqListStr, sizeof(freqListStr), " ", "%d", freqMHz);
+        }
+    }
     if(!swl_str_isEmpty(freqListStr)) {
-        swl_mapChar_add(global, "initial_freq_list", freqListStr);
         swl_mapChar_add(global, "freq_list", freqListStr);
     }
 
@@ -181,12 +185,6 @@ static swl_rc_ne s_setWpaSuppNetworkConfig(T_EndPoint* pEP, wld_wpaSupp_config_t
     ASSERT_FALSE(ret, SWL_RC_ERROR, ME, "empty SSID");
 
     swl_mapChar_add(network, "scan_ssid", "1");
-    char freqListStr[320] = {'\0'};
-    wld_rad_printPossibleFreqsWithSep(pRad, freqListStr, sizeof(freqListStr), " ");
-    if(!swl_str_isEmpty(freqListStr)) {
-        swl_mapChar_add(network, "scan_freq", freqListStr);
-        swl_mapChar_add(network, "freq_list", freqListStr);
-    }
     char ssid[SSID_NAME_LEN + 2] = {0};
     swl_str_catFormat(ssid, sizeof(ssid), "\"%s\"", epProfile->SSID);
     swl_mapChar_add(network, "ssid", ssid);
