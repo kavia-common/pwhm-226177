885e80ed9b7853c1bb09ca84654e2d0c447e64c9
Quentin Feraboli <quentin.feraboli@softathome.com>
2025-06-27 14:30:40 +0200

    pwhm: wpa_supplicant: fix: Fix use of uninitialized value
    As reported by valgrind during unit tests.
    
    GitOrigin-RevId: f372410e30ae2f2a98456f2d0530dcdee165faa1
    
    Implements: PPM-3681
    References: FEAT-117
    Signed-off-by: Quentin Feraboli <quentin.feraboli@softathome.com>
    Signed-off-by: Ahmed Amine HAOUES <ahmed-amine.haoues_ext@softathome.com>

--- src/nl80211/wld_wpaSupp_cfgFile.c
+++ src/nl80211/wld_wpaSupp_cfgFile.c
@@ -98,7 +98,7 @@ static swl_rc_ne s_setWpaSuppGlobalConfig(T_EndPoint* pEP, wld_wpaSupp_config_t*
     swl_mapChar_add(global, "model_number", pRad->wpsConst->ModelNumber);
     swl_mapChar_add(global, "serial_number", pRad->wpsConst->SerialNumber);
     swl_mapChar_add(global, "uuid", pRad->wpsConst->UUID);
-    int tmpver[4];
+    int tmpver[4] = {0};
     sscanf(pRad->wpsConst->OsVersion, "%i.%i.%i.%i", &tmpver[0], &tmpver[1], &tmpver[2], &tmpver[3]);
     swl_mapCharFmt_addValStr(global, "os_version", "%.8x", ((unsigned int) (tmpver[0] << 24 | tmpver[1] << 16 | tmpver[2] << 8 | tmpver[3])));
     swl_mapChar_add(global, "device_type", "6-0050F204-1");
