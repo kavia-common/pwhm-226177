b8ff5059a6847c0dcf019605fb032e9e588a73f0
Ahmed Amine HAOUES <ahmed-amine.haoues_ext@softathome.com>
2025-10-21 18:02:01 +0200

    pwhm: debug: print hostapd link status for debug
    Problem: generating debug information is missing MLD's links
    information.
    
    Cause: MLD's links are not handled
    
    Fix: loop over all MLD's links to print hostapd debug informations about
    each link.
    
    GitOrigin-RevId: d3138a79c65728dbd32f7791f0144a055ab66c1c
    
    ExternalID: SSW-11099
    
    Implements: PPM-3604
    
    References: FEAT-125
    
    Signed-off-by: Ahmed Amine HAOUES <ahmed-amine.haoues_ext@softathome.com>

--- scripts/debugInfo.sh
+++ scripts/debugInfo.sh
@@ -30,10 +30,22 @@ done
 interfaces=$(iw dev | grep Interface | awk '{print $2}' | sort)
 for INTF in $interfaces; do
   echo ""
-  echo "#### hostapd state ${INTF}"
-  printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} driver_flags"
-  printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} get_config"
-  printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} status"
-  printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} all_sta"
-  printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} wps_get_status"
+  links=$(iw ${INTF} info | grep link | awk -F: '{print $1}' | cut -d " " -f 2)
+  if [ -z "${links}" ]; then
+    echo "#### hostapd state ${INTF}"
+    printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} driver_flags"
+    printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} get_config"
+    printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} status"
+    printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} all_sta"
+    printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} wps_get_status"
+  else
+    for link in ${links}; do
+      echo "#### hostapd state ${INTF} link${link}"
+      printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} -l ${link} driver_flags"
+      printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} -l ${link} get_config"
+      printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} -l ${link} status"
+      printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} -l ${link} all_sta"
+      printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} -l ${link} wps_get_status"
+    done
+  fi
 done
