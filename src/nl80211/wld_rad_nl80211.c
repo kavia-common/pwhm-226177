f1b338cf4d04ad2a5cca54bb640c4e39fb0f7b79
Sagar Reddy <snandanuri@maxlinear.com>
2025-12-03 22:38:34 -0800

    wld_rad_nl80211: guard negative delta in other_bss_time calculation
    Issue & Cause:
    Corruption of OBSS and its respective time metrics in AirStats.
    
    Root Cause:
    In rare edge cases with MxL's WLAN driver, the time metrics provided in
    the NL80211 survey info can exhibit a drift of less than 1ms per sample.
    Since AirStats computation involves delta between current and last survey
    info, the drift is doubled (<2ms). This can result in **delta timeRx <
    delta timeRxInBss**. Since both are unsigned integers, this leads to an
    underflow in the calculation of `other_bss_time`. This underflow can
    propagate, causing further over or underflows in subsequent calculations,
    corrupting the air stats.
    
    Fix:
    Add a guard to check if `timeRx >= timeRxInBss` before performing the
    subtraction. If the condition is not met, set `other_bss_time` to 0 and
    log an error with the relevant values for debugging. This prevents
    underflow and ensures that the air stats remain consistent even in the
    presence of driver time drift.
    
    fixes PPW-1268
    
    Signed-off-by: Sagar Reddy <snandanuri@maxlinear.com>

--- src/nl80211/wld_rad_nl80211.c
+++ src/nl80211/wld_rad_nl80211.c
@@ -414,7 +414,13 @@ swl_rc_ne wld_rad_nl80211_getAirStatsFromSurveyInfo(T_Radio* pRadio, wld_airStat
     pStats->total_time = base; // eqv. all pChanSurveyInfo->timeOn;
     pStats->bss_transmit_time = (pChanSurveyInfo->timeTx * base) / pChanSurveyInfo->timeOn;
     pStats->bss_receive_time = (pChanSurveyInfo->timeRxInBss * base) / pChanSurveyInfo->timeOn;
-    pStats->other_bss_time = ((pChanSurveyInfo->timeRx - pChanSurveyInfo->timeRxInBss) * base) / pChanSurveyInfo->timeOn;
+    if(pChanSurveyInfo->timeRx >= pChanSurveyInfo->timeRxInBss) {
+        pStats->other_bss_time = ((pChanSurveyInfo->timeRx - pChanSurveyInfo->timeRxInBss) * base) / pChanSurveyInfo->timeOn;
+    } else {
+        pStats->other_bss_time = 0;
+        SAH_TRACEZ_ERROR(ME, "%s: timeRx(diff) %d < timeRxInBss(diff) %d",
+                         pRadio->Name, pChanSurveyInfo->timeRx, pChanSurveyInfo->timeRxInBss);
+    }
     uint64_t wifiTime = pChanSurveyInfo->timeTx + pChanSurveyInfo->timeRx + pChanSurveyInfo->timeScan;
     if(pChanSurveyInfo->timeBusy > wifiTime) {
         pStats->other_time = ((pChanSurveyInfo->timeBusy - wifiTime) * base) / pChanSurveyInfo->timeOn;
